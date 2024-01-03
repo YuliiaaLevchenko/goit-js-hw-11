5eec84a5e975dd1b6ab54ad6e754ec3d8fb25a99
YuliiaaLevchenko <141173167+YuliiaaLevchenko@users.noreply.github.com>
2024-01-03 16:20:17 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -3,3 +3,24 @@ import "izitoast/dist/css/iziToast.min.css";
 
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+const url = new URL("https://pixabay.com/api/");
+
+url.searchParams.append("key", "41611765-95d63315582311c92a96eb89d")
+url.searchParams.append("q", "cat")
+url.searchParams.append("image_type", "photo")
+url.searchParams.append("orientation", "horizontal")
+url.searchParams.append("safesearch", "true")
+
+fetch(url)
+    .then(res => {
+        if (!res.ok) {
+            throw new Error(
+                'Sorry, there are no images matching your search query. Please try again!'
+            );
+        }
+       return res.json();
+    })
+    .then(image => {
+        console.log(image);
+    })
